42a90081e1cdd26e3e19416fbdbdec073d98eb6e
rudmary <moncay.ruddy@gmail.com>
2018-07-02 15:21:48 -0500

    caja blanca

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 import unittest
@@ -7,8 +7,40 @@ import clasificacion_clientes
 class Test(unittest.TestCase):
 
 	def test_clasificacion_ID(self):
-		resultado = ""
-		self.assertEqual(resultado, "")
+		def test_clasificacion_1(self):
+			montoDepositado = 60000
+			informacionBuro = True
+			resultado = clasificacion_clientes.clasificacion_clientes(montoDepositado, informacionBuro)
+			self.assertEqual(resultado, "Tipo de cliente: AAA, Envio de boletin: Si")
 
+		def test_clasificacion_2(self):
+			montoDepositado = 30000
+			informacionBuro = True
+			resultado = clasificacion_clientes.clasificacion_clientes(montoDepositado, informacionBuro)
+			self.assertEqual(resultado, "Tipo de cliente: A, Envio de boletin: Si")
+
+		def test_clasificacion_3(self):
+			montoDepositado = 15000
+			informacionBuro = True
+			resultado = clasificacion_clientes.clasificacion_clientes(montoDepositado, informacionBuro)
+			self.assertEqual(resultado, "Tipo de cliente: A, Envio de boletin: Si")
+
+		def test_clasificacion_4(self):
+			montoDepositado = 10000
+			informacionBuro = True
+			resultado = clasificacion_clientes.clasificacion_clientes(montoDepositado, informacionBuro)
+			self.assertEqual(resultado, "Tipo de cliente: B, Envio de boletin: Si")
+
+		def test_clasificacion_5(self):
+			montoDepositado = 5000
+			informacionBuro = True
+			resultado = clasificacion_clientes.clasificacion_clientes(montoDepositado, informacionBuro)
+			self.assertEqual(resultado, "Tipo de cliente: C, Envio de boletin: Si")
+
+		def test_clasificacion_6(self):
+			montoDepositado = 4000
+			informacionBuro = False
+			resultado = clasificacion_clientes.clasificacion_clientes(montoDepositado,informacionBuro)
+			self.assertEqual(resultado, "Tipo de cliente: no es digno de crédito, Envio de boletin: No")
 if __name__ == '__main__':
 	unittest.main()
