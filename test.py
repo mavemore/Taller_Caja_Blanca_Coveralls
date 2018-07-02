24f7368e2bf2c3b17f3b130b73294c08e96b64f9
Bryan Tumbaco Moreira <bryandario1994@gmail.com>
2018-07-02 14:25:34 -0500

    primera version

--- test.py
+++ test.py
@@ -7,8 +7,9 @@ import clasificacion_clientes
 class Test(unittest.TestCase):
 
 	def test_clasificacion_ID(self):
-		resultado = ""
-		self.assertEqual(resultado, "")
+		resultado = clasificacion_clientes.clasificacion_clientes(30000, True)
+		mensaje = "Tipo de cliente: AAA, Envio de boletin: Si"
+		self.assertEqual(resultado, mensaje)
 
 if __name__ == '__main__':
 	unittest.main()
