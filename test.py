88b5f7e3a0447cd39e31b1c5917b833385d42cd0
jgorotiz <jgorotiz@espol.edu.ec>
2018-07-02 14:35:51 -0500

    Añadiendo un test

--- test.py
+++ test.py
@@ -2,13 +2,13 @@
 from __future__ import unicode_literals
 
 import unittest
-import clasificacion_clientes
+from clasificacion_clientes import clasificacion_clientes
 
 class Test(unittest.TestCase):
 
-	def test_clasificacion_ID(self):
-		resultado = ""
-		self.assertEqual(resultado, "")
+	def test_clasificacion_ID_1(self):
+		resultado = clasificacion_clientes(1000,False)
+		self.assertEqual(resultado, "Tipo de cliente: no es digno de crédito, Envio de boletin: No" )
 
 if __name__ == '__main__':
 	unittest.main()
