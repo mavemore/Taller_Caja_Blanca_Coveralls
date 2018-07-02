fd01d80c3bfcc90d894ddc9ea4cacde4e9b8b674
Joel Rodriguez <joelerll@gmail.com>
2018-07-02 14:30:37 -0500

    cambio

--- test.py
+++ test.py
@@ -6,9 +6,21 @@ import clasificacion_clientes
 
 class Test(unittest.TestCase):
 
-	def test_clasificacion_ID(self):
-		resultado = ""
-		self.assertEqual(resultado, "")
+	def test_clasificacion_ID1(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(30000, True)
+		self.assertEqual(resultado, "Tipo de cliente: AAA, Envio de boletin: Si")
+	def test_clasificacion_ID2(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(20000, True)
+		self.assertEqual(resultado, "Tipo de cliente: AA, Envio de boletin: Si")
+	def test_clasificacion_ID3(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(15000, True)
+		self.assertEqual(resultado, "Tipo de cliente: A, Envio de boletin: Si")
+	def test_clasificacion_ID4(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(10000, True)
+		self.assertEqual(resultado, "Tipo de cliente: B, Envio de boletin: Si")
+	def test_clasificacion_ID5(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(5000, True)
+		self.assertEqual(resultado, "Tipo de cliente: C, Envio de boletin: Si")
 
 if __name__ == '__main__':
 	unittest.main()
