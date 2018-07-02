867db89b50c031edb0af6022f200940462222123
MariaBelenGuaranda <mguarand@espol.edu.ec>
2018-07-02 14:37:48 -0500

    tested when AAA

--- test.py
+++ test.py
@@ -10,6 +10,10 @@ class Test(unittest.TestCase):
 		resultado = clasificacion_clientes(2000,False)
 		self.assertEqual(resultado, "Tipo de cliente: no es digno de crédito, Envio de boletin: No" )
 
+	def test_clasificacion_AAA(self):
+		resultado = clasificacion_clientes(30000,False)
+		self.assertEqual(resultado, "Tipo de cliente: AAA, Envio de boletin: Si" )
+
 
 if __name__ == '__main__':
 	unittest.main()
