e89a40692c83c45534905df6cfd0940f3725e196
jgorotiz <jgorotiz@espol.edu.ec>
2018-07-02 14:42:03 -0500

    Prueba

--- test.py
+++ test.py
@@ -9,6 +9,9 @@ class Test(unittest.TestCase):
 	def test_clasificacion_ID_1(self):
 		resultado = clasificacion_clientes(1000,False)
 		self.assertEqual(resultado, "Tipo de cliente: no es digno de crédito, Envio de boletin: No" )
+	def test_clasificacion_ID_2(self):
+		resultado = clasificacion_clientes(31000,True)
+		self.assertEqual(resultado, "Tipo de cliente: AAA, Envio de boletin: Si" )
 
 if __name__ == '__main__':
 	unittest.main()
