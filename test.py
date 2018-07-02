097296cd5a2051159f3e8d81e7c79e9bdc55a31b
dajocape <danijo1995@gmail.com>
2018-07-02 14:32:45 -0500

    Corrección de identificadores de test

--- test.py
+++ test.py
@@ -6,9 +6,29 @@ import clasificacion_clientes
 
 class Test(unittest.TestCase):
 
-	def test_clasificacion_ID(self):
-		resultado = ""
-		self.assertEqual(resultado, "")
+	def test_clasificacion_1(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(5000, False)
+		self.assertEqual(resultado, "Tipo de cliente: no es digno de crédito, Envio de boletin: No")
+		
+	def test_clasificacion_2(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(31000, True)
+		self.assertEqual(resultado, "Tipo de cliente: AAA, Envio de boletin: Si")
+		
+	def test_clasificacion_3(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(22000, True)
+		self.assertEqual(resultado, "Tipo de cliente: AA, Envio de boletin: Si")
+		
+	def test_clasificacion_4(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(15000, True)
+		self.assertEqual(resultado, "Tipo de cliente: A, Envio de boletin: Si")
+		
+	def test_clasificacion_5(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(12000, True)
+		self.assertEqual(resultado, "Tipo de cliente: B, Envio de boletin: Si")
+	
+	def test_clasificacion_6(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(7000, True)
+		self.assertEqual(resultado, "Tipo de cliente: C, Envio de boletin: Si")
 
 if __name__ == '__main__':
 	unittest.main()
