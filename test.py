042a46ecdda2f630e6399ddaf7d22580b9562f18
Cliente CONTROL-PC <CltControl@GGIECSOFWRK147.espol.edu.ec>
2018-07-02 13:58:28 -0500

    cambio

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import unittest
+import clasificacion_clientes
+
+class Test(unittest.TestCase):
+
+	def test_clasificacion_ID(self):
+		resultado = ""
+		self.assertEqual(resultado, "")
+
+if __name__ == '__main__':
+	unittest.main()
