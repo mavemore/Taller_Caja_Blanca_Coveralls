1ce60739d42fa38ebe80b17860a249b256fc25b2
Cliente CONTROL-PC <CltControl@GGIECSOFWRK144.espol.edu.ec>
2018-07-02 14:30:13 -0500

    Test

--- test.py
+++ test.py
@@ -4,11 +4,37 @@ from __future__ import unicode_literals
 import unittest
 import clasificacion_clientes
 
+
 class Test(unittest.TestCase):
 
-	def test_clasificacion_ID(self):
-		resultado = ""
-		self.assertEqual(resultado, "")
+	def test_clasificacion_1(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(40000,True)
+		self.assertEqual(resultado, "Tipo de cliente: AAA, Envio de boletin: Si")
+
+	def test_clasificacion_2(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(29000,True)
+		self.assertEqual(resultado, "Tipo de cliente: AA, Envio de boletin: Si")
+
+	def test_clasificacion_3(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(16000,True)
+		self.assertEqual(resultado, "Tipo de cliente: A, Envio de boletin: Si")
+
+	def test_clasificacion_4(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(11000,True)
+		self.assertEqual(resultado, "Tipo de cliente: B, Envio de boletin: Si")
+
+	def test_clasificacion_5(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(6000,True)
+		self.assertEqual(resultado, "Tipo de cliente: C, Envio de boletin: Si")
+
+	def test_clasificacion_6(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(35000,False)
+		self.assertEqual(resultado, "Tipo de cliente: no es digno de crédito, Envio de boletin: No")
+
+	def test_clasificacion_7(self):
+		resultado = clasificacion_clientes.clasificacion_clientes(1000,False)
+		self.assertEqual(resultado, "Tipo de cliente: no es digno de crédito, Envio de boletin: No")
+
 
 if __name__ == '__main__':
 	unittest.main()
