0cb4b618e78e5cfc2d4aac01ccc5fc6e0bc82d0c
jgorotiz <jgorotiz@espol.edu.ec>
2018-07-02 14:47:53 -0500

    Casos finales

--- test.py
+++ test.py
@@ -12,6 +12,18 @@ class Test(unittest.TestCase):
 	def test_clasificacion_ID_2(self):
 		resultado = clasificacion_clientes(31000,True)
 		self.assertEqual(resultado, "Tipo de cliente: AAA, Envio de boletin: Si" )
+	def test_clasificacion_ID_3(self):
+		resultado = clasificacion_clientes(25000,True)
+		self.assertEqual(resultado, "Tipo de cliente: AA, Envio de boletin: Si" )
+	def test_clasificacion_ID_4(self):
+		resultado = clasificacion_clientes(15000,True)
+		self.assertEqual(resultado, "Tipo de cliente: A, Envio de boletin: Si" )
+	def test_clasificacion_ID_5(self):
+		resultado = clasificacion_clientes(10000,True)
+		self.assertEqual(resultado, "Tipo de cliente: B, Envio de boletin: Si" )
+	def test_clasificacion_ID_6(self):
+		resultado = clasificacion_clientes(5000,True)
+		self.assertEqual(resultado, "Tipo de cliente: C, Envio de boletin: Si" )
 
 if __name__ == '__main__':
 	unittest.main()
